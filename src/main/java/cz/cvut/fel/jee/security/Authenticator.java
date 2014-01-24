c064ee5513edad3c6c088916fbbfdb17a062e01e
Tomáš Turek <turekto5@fel.cvut.cz>
2014-01-24 20:18:12 +0100

    logování exception v Authenticator

--- src/main/java/cz/cvut/fel/jee/security/Authenticator.java
+++ src/main/java/cz/cvut/fel/jee/security/Authenticator.java
@@ -1,7 +1,7 @@
 package cz.cvut.fel.jee.security;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import cz.cvut.fel.jee.annotations.CurrentLoggedUser;
+import cz.cvut.fel.jee.model.User;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.SessionScoped;
@@ -10,69 +10,83 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-
-import cz.cvut.fel.jee.annotations.CurrentLoggedUser;
-import cz.cvut.fel.jee.model.User;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Named(value = "authenticator")
 @Stateful
 @SessionScoped
-public class Authenticator {
+public class Authenticator implements Serializable
+{
 
-	@Inject
-	EntityManager em;
-	
-	private String password;
-	private String email;
-	private User currentUser;
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String username) {
-		this.email = username;
-	}
-	
-	public void login(){
-		TypedQuery<User> query = em.createNamedQuery("User.findByEmail",User.class).setParameter("email",email);
+    @Inject
+    private transient EntityManager em;
+
+    @Inject
+    private transient Logger logger;
+
+    private String password;
+
+    private String email;
+
+    private User currentUser;
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String username)
+    {
+        this.email = username;
+    }
+
+    public void login()
+    {
+        TypedQuery<User> query = em.createNamedQuery("User.findByEmail", User.class).setParameter("email", email);
         User user = query.getSingleResult();
         try {
-        	MessageDigest md = MessageDigest.getInstance("SHA-256");
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
             String newPassword = password + email;
             md.update(newPassword.getBytes());
             byte[] pwd = md.digest();
             StringBuffer sb = new StringBuffer();
             for (int i = 0; i < pwd.length; i++) {
-                    sb.append(Integer.toString((pwd[i] & 0xff) + 0x100, 16)
-                                    .substring(1));
+                sb.append(Integer.toString((pwd[i] & 0xff) + 0x100, 16)
+                        .substring(1));
             }
             newPassword = sb.toString();
-            if(user.getPassword().equals(newPassword)){
-            	currentUser=user;
-            	password = null;
-            	email = null;
-            }else{
-            	//@todo vyjimka, messages
+            if (user.getPassword().equals(newPassword)) {
+                currentUser = user;
+                password = null;
+                email = null;
+            } else {
+                //@todo vyjimka, messages
             }
-		} catch (NoSuchAlgorithmException e) {
-			//Takovej algoritmus JE!
-		}
-	}
-	
-	@Produces
-	@CurrentLoggedUser
-	public User getCurrentUser() {
-		return currentUser;
-	}
-		
+        } catch (NoSuchAlgorithmException e) {
+            logger.log(Level.SEVERE, "an exception was thrown", e);
+        }
+    }
+
+    @Produces
+    @CurrentLoggedUser
+    public User getCurrentUser()
+    {
+        return currentUser;
+    }
+
 
 }
