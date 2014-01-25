01e6ba14843f5534558b19d996660665f4f0eb00
Saljack <saljacky@gmail.com>
2014-01-25 17:56:21 +0100

    Video upload and infinispan

--- src/main/java/cz/cvut/fel/jee/beans/VideoUpload.java
+++ src/main/java/cz/cvut/fel/jee/beans/VideoUpload.java
@@ -5,18 +5,24 @@
  */
 package cz.cvut.fel.jee.beans;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import javax.servlet.http.Part;
+import org.infinispan.io.GridFilesystem;
 
 /**
  *
@@ -28,6 +34,12 @@ public class VideoUpload implements Serializable {
 
     private static final String videoMimeTypes[] = {"video/avi", "video/msvideo", "video/x-msvideo", "video/mp4", "video/mpeg", "video/x-matroska", "video/webm"};
     Part video;
+
+    @Inject
+    GridFilesystem fileSystem;
+
+    @Inject
+    private transient Logger logger;
 
     /**
      * Creates a new instance of VideoUpload
@@ -47,13 +59,28 @@ public class VideoUpload implements Serializable {
         try {
             if (video != null) {
 
-                video.write(video.getSubmittedFileName());
-                System.out.println("FILE SAVED");
+                //TODO create new video entity
+                Long id = 8L;
+                File dir = fileSystem.getFile("/video/uploaded");
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                    logger.info("Creating dirs");
+                }
+                InputStream is = video.getInputStream();
+                OutputStream os = fileSystem.getOutput("/video/uploaded/" + id + ".avi");
+                byte[] buffer = new byte[20000];
+                int len;
+                while ((len = is.read(buffer, 0, buffer.length)) != -1) {
+                    os.write(buffer, 0, len);
+                }
+                is.close();
+                os.close();
+                logger.info("File is writed");
             }
         } catch (IOException e) {
-            // Error handling
-            System.err.println("Error");
+            logger.warning(e.toString());
         }
+        
         return "";
     }
 
--- src/main/java/cz/cvut/fel/jee/utils/Resources.java
+++ src/main/java/cz/cvut/fel/jee/utils/Resources.java
@@ -1,6 +1,5 @@
 package cz.cvut.fel.jee.utils;
 
-import javax.ejb.Stateless;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
@@ -9,33 +8,46 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import org.infinispan.Cache;
+import org.infinispan.io.GridFile;
+import org.infinispan.io.GridFilesystem;
+import org.infinispan.manager.CacheContainer;
 
 /**
  * Created by Tomáš on 10.1.14.
  */
-public class Resources
-{
+public class Resources {
 
     @PersistenceContext(unitName = "primary")
     private EntityManager em;
 
+    @Resource(lookup = "java:jboss/infinispan/container/video")
+    private CacheContainer container;
+
     @Produces
-    public EntityManager getEm()
-    {
+    public EntityManager getEm() {
         return em;
     }
 
     @Produces
-    public Logger getLogger(InjectionPoint ip)
-    {
+    public Logger getLogger(InjectionPoint ip) {
         String category = ip.getMember().getDeclaringClass().getName();
         return Logger.getLogger(category);
     }
 
     @Produces
     @RequestScoped
-    public FacesContext getFacesContext()
-    {
+    public FacesContext getFacesContext() {
         return FacesContext.getCurrentInstance();
+    }
+
+    @Produces
+    public GridFilesystem getVideoCache() {
+        Cache<String, byte[]> data = container.getCache("distributed");
+        Cache<String, GridFile.Metadata> metadata = container.getCache("replicated");
+        GridFilesystem fs = new GridFilesystem(data, metadata);
+        return fs;
     }
 }
