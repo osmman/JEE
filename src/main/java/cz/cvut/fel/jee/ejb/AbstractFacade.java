a0c9a2dbc5a86d629a8e99879e52e13915d844a3
Tomáš Turek <turekto5@fel.cvut.cz>
2014-01-25 16:18:13 +0100

    Rest resource user

--- src/main/java/cz/cvut/fel/jee/ejb/AbstractFacade.java
+++ src/main/java/cz/cvut/fel/jee/ejb/AbstractFacade.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.jee.ejb;
 
+import cz.cvut.fel.jee.model.EntityObject;
 import cz.cvut.fel.jee.utils.QueryBuilder;
 
 import javax.inject.Inject;
@@ -14,7 +15,7 @@ import java.util.List;
 /**
  * Created by Tomáš on 24.1.14.
  */
-public abstract class AbstractFacade<T>
+public abstract class AbstractFacade<T extends EntityObject>
 {
 
     private Class<T> entityClass;
