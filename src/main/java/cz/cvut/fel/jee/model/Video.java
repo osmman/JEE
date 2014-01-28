16523fd20f0ba537c5e6b99875077dddb92f33fe
Bc. Vlastimil Jinoch <jinocvla@fel.cvut.cz>
2014-01-28 21:51:05 +0100

    Add callback and published field to video entity.

--- src/main/java/cz/cvut/fel/jee/model/Video.java
+++ src/main/java/cz/cvut/fel/jee/model/Video.java
@@ -1,13 +1,9 @@
 package cz.cvut.fel.jee.model;
 
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Set;
-import javax.persistence.OrderBy;
 
 /**
  * Created by Tomáš on 21.1.14.
@@ -23,6 +19,8 @@ public class Video extends EntityObject
     private String path;
     
     private String mimetype;
+
+    private Boolean published;
 
     @NotNull
     @ManyToOne
@@ -93,4 +91,11 @@ public class Video extends EntityObject
         this.mimetype = mimetype;
     }
 
+    public Boolean getPublished() {
+        return published;
+    }
+
+    public void setPublished(Boolean published) {
+        this.published = published;
+    }
 }
