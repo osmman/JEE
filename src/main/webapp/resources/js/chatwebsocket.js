c7e03b1b9a12da24b84b913ac969818566fa4ca2
Tomáš Turek <osmman@gmail.com>
2014-01-29 10:36:42 +0100

    Oprava websocket chat javascript

--- src/main/webapp/resources/js/chatwebsocket.js
+++ src/main/webapp/resources/js/chatwebsocket.js
@@ -1,40 +1,39 @@
+$.urlParam = function (name) {
+    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
 
-$(function(){
-    $.urlParam = function(name) {
-        var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
-        return results[1] || 0;
-    }
+var url = "ws://" + document.location.host + "/semestralka/chat/" + $.urlParam("videoid");
+var websocket = new WebSocket(url);
 
-    var url = "ws://" + document.location.host + "/semestralka/chat/" + $.urlParam("videoid");
-    var websocket = new WebSocket(url);
+console.log(document.location.host);
+console.log(document.location.pathname);
 
-    console.log(document.location.host);
-    console.log(document.location.pathname);
+websocket.onmessage = function (evt) {
+    onMessage(evt);
+};
 
-    websocket.onmessage = function(evt) {
-        onMessage(evt);
-    };
+websocket.onerror = function (evt) {
+    console.log('ERROT')
+};
 
-    websocket.onerror = function(evt) {console.log('ERROT')};
+function send() {
+    var message = {"text": $("#formmessage\\:message").val()};
+    var jsonString = JSON.stringify(message);
+    console.log(jsonString);
+    websocket.send(jsonString);
+}
 
-    function send() {
-        var message = {"text": $("#formmessage\\:message").val()};
-        var jsonString = JSON.stringify(message);
-        console.log(jsonString);
-        websocket.send(jsonString);
-    }
+function onMessage(evt) {
+    var json = JSON.parse(evt.data);
+    console.log(" " + evt.data);
 
-    function onMessage(evt) {
-        var json = JSON.parse(evt.data);
-        console.log(" " + evt.data);
+    var newMessage = $('<div class="media">'
+        + '<div class="media-body">'
+        + '<h4 class="media-heading">' + json.author
+        + '</h4>'
+        + json.text + '</br>'
+        + json.date);
 
-        var newMessage = $('<div class="media">'
-            + '<div class="media-body">'
-            + '<h4 class="media-heading">' + json.author
-            + '</h4>'
-            + json.text + '</br>'
-            + json.date);
-
-        $("#chat-body").append(newMessage);
-    }
-})
+    $("#chat-body").append(newMessage);
+}
