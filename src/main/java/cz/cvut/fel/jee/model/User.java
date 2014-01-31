b2e9ad96ef9f57cf6e5171668fb140528948a38a
Saljack <saljacky@gmail.com>
2014-01-31 16:29:35 +0100

    User merge

--- src/main/java/cz/cvut/fel/jee/model/User.java
+++ src/main/java/cz/cvut/fel/jee/model/User.java
@@ -14,13 +14,12 @@ import org.hibernate.validator.constraints.Email;
  * Created by Tomáš on 10.1.14.
  */
 @NamedQueries({
-        @NamedQuery(name = "User.findByEmail", query = "SELECT s FROM User s WHERE s.email like :email")
+    @NamedQuery(name = "User.findByEmail", query = "SELECT s FROM User s WHERE s.email like :email")
 })
 @Table(name = "users")
 @Entity
 @XmlJavaTypeAdapter(UserAdapter.class)
-public class User extends EntityObject
-{
+public class User extends EntityObject {
 
     private static final long serialVersionUID = 7434469730098060563L;
 
@@ -34,6 +33,9 @@ public class User extends EntityObject
 
     @OneToMany(mappedBy = "author")
     private Set<Comment> comments;
+
+    @ManyToMany
+    private Set<Role> roles;
 
     @Email
     @NotNull
@@ -50,18 +52,8 @@ public class User extends EntityObject
         return password;
     }
 
-    public void setPassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        String newPassword = password + email;
-        md.update(newPassword.getBytes());
-        byte[] pwd = md.digest();
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < pwd.length; i++) {
-            sb.append(Integer.toString((pwd[i] & 0xff) + 0x100, 16)
-                    .substring(1));
-        }
-        newPassword = sb.toString();
-        this.password = newPassword;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Set<Video> getVideos() {
@@ -79,6 +71,13 @@ public class User extends EntityObject
     public void setComments(Set<Comment> comments) {
         this.comments = comments;
     }
-    
-    
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
 }
