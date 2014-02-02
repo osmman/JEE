9b180d41785551523f2fbc329f9219b8648f1665
Tomáš Turek <osmman@gmail.com>
2014-02-02 22:55:25 +0100

    FIX #26 - V administraci u users.xhtml nejaka hlaska

--- src/main/java/cz/cvut/fel/jee/security/RolesHelper.java
+++ src/main/java/cz/cvut/fel/jee/security/RolesHelper.java
@@ -1,9 +1,11 @@
 package cz.cvut.fel.jee.security;
 
+import cz.cvut.fel.jee.annotations.CurrentLoggedUser;
 import cz.cvut.fel.jee.model.Role;
 import cz.cvut.fel.jee.model.User;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
@@ -13,6 +15,10 @@ import javax.inject.Named;
 public class RolesHelper {
 
     private Role admin;
+
+    @Inject
+    @CurrentLoggedUser
+    private User user;
 
     @PostConstruct
     private void init(){
@@ -24,7 +30,12 @@ public class RolesHelper {
         return hasRole(user, Role.ADMIN);
     }
 
+    public boolean isAdmin(){
+        return hasRole(user, Role.ADMIN);
+    }
+
     public boolean hasRole(User user,String role){
+        if(user == null) return false;
         return user.getRoles().contains(admin);
     }
 }
